8341b5ad463bc0e033f9440131e6fbc314017433
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-20 20:45:05 -0300

    feat(front/react): Route exercise structure

---
+++ Wellington Cidade/React/Rotas/loja/src/Components/Menu.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+export default function Menu(){
+
+
+    return(
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/moda-masculina">Moda Masculina</Link></li>
+                <li><Link to="/moda-feminina">Moda Feminina</Link></li>
+                <li><Link to="/moda-infantil">Moda Infantil</Link></li>
+                <li><Link to="/sobre">Sobre</Link></li>
+            </ul>
+        </nav>
+    )
+}
