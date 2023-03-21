83152231351c7c9a0496323c9ac3c970d484e882
Vinícius Yoda <vyoda4604@gmail.com>
2023-03-21 20:01:50 -0300

    feat(front): implement layout loja project

--- Wellington Cidade/React/Rotas/loja/src/Components/Footer.js
+++ Wellington Cidade/React/Rotas/loja/src/Components/Footer.js
@@ -1,11 +1,11 @@
-// import {Link} from 'react-router-dom'
+import '../css/Footer.css'
 
-export default function Footer(){
-
-
-    return(
-        <>
-            
-        </>
-    )
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Arthur Foschiani</p>
+      </footer>
+    </>
+  );
 }
